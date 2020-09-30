f87d25b8ec6e80e85bd62fd0d8995db527d4ed83
rado <radzik.p@gmail.com>
2020-09-30 20:37:47 +0200

    Test cpu block mine

---
+++ blockchain/blockchain-tst/src/main/java/com/panowek/blockchain/Block.java
@@ -0,0 +1,40 @@
+package com.panowek.blockchain;
+
+import com.panowek.utils.StringUtil;
+import lombok.Getter;
+
+import java.util.Date;
+
+@Getter
+public class Block {
+    private String hash;
+    private final String previousHash;
+    private final String data; //our data will be a simple message.
+    private final long timeStamp; //as number of milliseconds since 1/1/1970.
+    private int nonce;
+
+    public Block(String data,String previousHash ) {
+        this.data = data;
+        this.previousHash = previousHash;
+        this.timeStamp = new Date().getTime();
+        this.hash = calculateHash(); //Making sure we do this after we set the other values.
+    }
+
+    public String calculateHash() {
+        String calculatedhash = StringUtil.applySha256(
+                this.previousHash + Long.toString(this.timeStamp) + this.data
+        );
+        return calculatedhash;
+    }
+
+    public void mineBlock(int difficulty) {
+        String target = new String(new char[difficulty]).replace('\0', '0'); //Create a string with difficulty * "0"
+        while(!this.hash.substring( 0, difficulty).equals(target)) {
+            this.nonce ++;
+            this.hash = calculateHash();
+        }
+        System.out.println("Block Mined!!! : " + hash);
+    }
+
+
+}
